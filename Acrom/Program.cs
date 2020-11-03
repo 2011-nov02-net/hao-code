4f3975b3b841d2e9062dcc3f2e07ea05ef7f7b18
Hao Yang <haoyang439@gmail.com>
2020-11-03 14:19:25 -0700

    Acrom exercise

---
+++ Acrom/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Acrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+        }
+    }
+}
