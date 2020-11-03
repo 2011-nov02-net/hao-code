8ad64bb5adb1766ad43104439a00c73b1016e752
Hao Yang <haoyang439@gmail.com>
2020-11-03 16:01:11 -0700

    Acrom update

--- Acrom/Program.cs
+++ Acrom/Program.cs
@@ -6,8 +6,16 @@ namespace Acrom
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a few words with spaces");
             string input = Console.ReadLine();
-
+            string[] words = input.Split();
+            foreach (var word in words)
+            {
+                string key = $"{word[0]}";
+                // Console.Write(key);
+                string upper = key.ToUpper();
+                Console.Write(upper);
+            }
         }
     }
 }
