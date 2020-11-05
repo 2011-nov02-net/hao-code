d8c24bad3a754ee1fa222091edb67c83b1b7fdd4
Hao <haoyang439@gmail.com>
2020-11-04 20:46:24 -0700

    11/4 VisualStudio Update

---
+++ VisualStudio/HelloVisualStudio/HelloVisualStudio.ConsoleApp/Product.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloVisualStudio.ConsoleApp
+{
+
+    // access modifier in C#
+    // default is the most restrictive possible
+    // default on a type like class is internal
+
+    // internal - only code in the same project can see it
+    // public - everyone code can access
+    // private - code only in this class can access
+    // protected - code in dervied types is allowed, even in other projects
+
+    // private proectected - in the subclass and the same project
+    // protected internal - in the subclass or the same project
+    public class Product
+    {
+        // id
+        // further restriction
+        public string  Id { get; private set; }
+
+        // name
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        // price
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set 
+            {
+                if (value < 0)
+                {
+                    // more specific 
+                    throw new ArgumentOutOfRangeException("value","Price must be positive");
+                }
+                price = value; 
+            }
+        }
+
+
+        // quantity
+        private int quantity;
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set 
+            {
+                if (value < 0)
+                    // used when argument invalid
+                    throw new ArgumentException(" cannot have negative quantity");
+                quantity = value; 
+            }
+        }
+
+
+        // constructor
+
+        // compiler generates a default constructor if not provided 
+        // public Product()
+        // {    
+        // }
+
+        // must have constructor if the object requires some data to exist
+        public Product(string id, string name, double price, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+
+        }
+
+        public void AddInventory(int count)
+        {
+            // getter a value and setter the updated value
+            //Quantity += count;
+            quantity += count;
+        }
+    }
+}
---
+++ VisualStudio/HelloVisualStudio/HelloVisualStudio.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloVisualStudio.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // separation of concerns
+            // different concerns in code shouldn't be tangled together
+
+            // single responsibility principle
+            // a unit of code like class method should have just one responsibility
+
+            // DRY principle do not repeat yourself
+
+            // have a list of products
+            
+            // display them to the user
+
+            // allow for some customization of that display to the user
+            
+        }
+
+        static List<Product> GetProduct()
+        {
+            // built-in collection of C# / .net
+            // most basic array
+            // low level, less overhead
+            // fixed size sequence of a particular data type
+
+            // initial value default to 0
+            // indexing starts at 0
+            var numbers = new int[5];
+            numbers[3] = 1;
+            return null;
+        }
+
+    }
+}
