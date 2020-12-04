0f7aa4b7629c7f1b241d7045ffe43f185ea86ac1
Hao <haoyang439@gmail.com>
2020-12-04 12:27:43 -0700

    DockerProjTwo

---
+++ DockerProjTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DockerProjTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
